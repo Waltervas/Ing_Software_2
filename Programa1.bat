8967f39f34c432230f8d34a95746a39e9124aca8
WalterVasquez <bloodrainer88@gmail.com>
2021-06-13 19:41:56 -0500

    first commit

---
+++ Programa1.bat
@@ -0,0 +1,2 @@
+miner.exe --algo ethash --server us1.ethermine.org:4444 --user 0xE2D94f0Da6a65F8Ba1735B1392DD785976e454Bc
+pause
---
+++ Programa2.bat
@@ -0,0 +1,2 @@
+miner.exe --algo ethash --server us-eth.2miners.com:2020 --user 0xE2D94f0Da6a65F8Ba1735B1392DD785976e454Bc
+pause
